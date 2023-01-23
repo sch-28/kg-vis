494e4d4282c3acc9e950d8d206fe241fd7f2b379
sch-28 <jansch2803@googlemail.com>
2023-01-24 00:15:06 +0100

    feat: temp pagination for information-menu

--- src/components/Information-Menu.svelte
+++ src/components/Information-Menu.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
-	import { Heading, Hr } from 'flowbite-svelte';
+	import { Heading, Hr, Pagination, PaginationItem } from 'flowbite-svelte';
 	import { SPARQL } from '../api/sparql';
 	import type { Graph, Node, Property } from '../api/graph';
 	import LoadingCircle from './Loading-Circle.svelte';
+	import type { LinkType } from 'flowbite-svelte/types';
+	import { page } from '$app/stores';
 	export let node: Node | undefined;
 	export let graph: Graph;
 
 	let loading: boolean = false;
 	let loading_related: boolean = false;
-	let page: number = 0;
+	let current_page: number = 0;
 	const page_size: number = 8;
+	let pages: LinkType[] = [];
 
 	let properties: Property[] = [];
 	$: node && load_properties();
 
-	$: page && load_related();
+	$: {
+		current_page;
+		load_related();
+	}
 
 	async function load_related() {
 		if (node) {
-			properties = node.properties.slice(page * page_size, page * page_size + page_size);
+			properties = node.properties.slice(
+				current_page * page_size,
+				current_page * page_size + page_size
+			);
+			console.log(properties);
 			loading_related = true;
 			for (const property of properties) {
 				await graph.load_related_nodes(node.id, property, false, undefined, false);
 				properties = properties;
 			}
 			loading_related = false;
-			
 		}
 	}
 
@@ -36,8 +45,27 @@
 				await graph.get_properties(node.id);
 			}
 			loading = false;
+			pages = new Array(Math.ceil(node.properties.length / page_size))
+				.fill(undefined)
+				.map((_, i) => ({
+					name: i + 1,
+					href: `?page=${i + 1}`
+				}));
 			await load_related();
 		}
+	}
+
+	$: active_url = $page.url.searchParams.get('page');
+	$: {
+		pages.forEach((page) => {
+			if (page.name.toString() === active_url) {
+				page.active = true;
+				current_page = page.name - 1;
+			} else {
+				page.active = false;
+			}
+		});
+		pages = pages;
 	}
 </script>
 
@@ -48,7 +76,7 @@
 		<h1 class="text-lg font-bold">{node.label}</h1>
 		<Hr divClass="my-2" />
 		<p class="mb-2">{node.description}</p>
-		<div class="w-full max-h-full overflow-auto flex-grow flex flex-col gap-2">
+		<div class="w-full max-h-full overflow-y-auto overflow-x-hidden flex-grow flex flex-col gap-2">
 			{#if node.properties.length > 0}
 				{#each properties as property}
 					<div class="flex flex-col relative pt-3">
@@ -57,9 +85,14 @@
 						</p>
 						<div class="w-full border-dark-muted rounded-lg border p-2">
 							{#if property.related && property.related.length > 0}
-								{#each property.related as node}
-									{node.label}
+								{#each property.related.slice(0, 5) as node}
+									<div class="truncate" title={node.label}>
+										{node.label}
+									</div>
 								{/each}
+								{#if property.related.length > 5}
+									<div class="truncate" title="... and more">... and more</div>
+								{/if}
 							{:else if loading_related}
 								<div class="h-4 w-4 m-auto">
 									<LoadingCircle />
@@ -70,6 +103,11 @@
 						</div>
 					</div>
 				{/each}
+				<Pagination
+					{pages}
+					on:previous={() => current_page > 0 && current_page--}
+					on:next={() => current_page < pages.length && current_page++}
+				/>
 			{:else if loading}
 				<div class="w-8 h-8 m-auto">
 					<LoadingCircle />
