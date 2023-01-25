2863c188208550901676ed33cf4782a65c1acebe
sch-28 <sch-28@users.noreply.github.com>
2023-01-25 20:37:18 +0000

    deploy: 68966b4e5e42c7235ac74355e1c00b8051c97f47

---
+++ _app/immutable/chunks/2-03cbb8d0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-9aaad2c6.js";export{t as component};
--- _app/immutable/chunks/2-1b095aea.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-fcb3aeed.js";export{t as component};
