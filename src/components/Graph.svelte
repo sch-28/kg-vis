3a17c4764b8237c984773606984316a1135ff49d
sch-28 <jansch2803@googlemail.com>
2023-02-07 04:44:15 +0100

    feat: added graph progress controls

--- src/components/Graph.svelte
+++ src/components/Graph.svelte
@@ -5,6 +5,9 @@
 	import ActionMenu from './Header.svelte';
 	import InformationMenu from './Information-Menu.svelte';
 	import LoadingCircle from './util/Loading-Circle.svelte';
+	import { Button, Popover } from 'flowbite-svelte';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { Play } from '@steeze-ui/heroicons';
 
 	interface Click_Event {
 		edges: [];
@@ -90,10 +93,43 @@
 <ActionMenu {graph} />
 
 {#if loading_graph}
-	<div
-		class="absolute bottom-4 right-4 w-6 h-6 flex justify-center items-center"
-		title="Graph is stabilizing.."
-	>
+	<div class="absolute bottom-4 right-4 w-6 h-6 flex justify-center items-center">
 		<LoadingCircle />
+		<Popover class="w-44 text-sm font-normal [&>*:nth-child(2)]:p-0" title="Graph is stabilizing">
+			<div class="flex flex-col items-start">
+				<Button
+					on:click={() => graph.network.stabilize()}
+					color="light"
+					btnClass="py-2 px-3 w-full text-left dark:hover:bg-black/30 hover:bg-black/10"
+					>Skip animation</Button
+				>
+				<Button
+					on:click={() => graph.network.stopSimulation()}
+					color="light"
+					btnClass="rounded-b-lg py-2 px-3 w-full text-left dark:hover:bg-black/30 hover:bg-black/10"
+					>Stop</Button
+				>
+			</div>
+		</Popover>
+	</div>
+{:else}
+	<div class="absolute bottom-4 right-4 w-6 h-6 flex justify-center items-center">
+		<Icon src={Play} />
+		<Popover class="w-44 text-sm font-normal [&>*:nth-child(2)]:p-0" title="Graph is resting">
+			<div class="flex flex-col items-start">
+				<Button
+					on:click={() => graph.network.startSimulation()}
+					color="light"
+					btnClass="py-2 px-3 w-full text-left dark:hover:bg-black/30 hover:bg-black/10"
+					>Start animation</Button
+				>
+				<Button
+					on:click={() => graph.network.stabilize()}
+					color="light"
+					btnClass="rounded-b-lg py-2 px-3 w-full text-left dark:hover:bg-black/30 hover:bg-black/10"
+					>Stabilize</Button
+				>
+			</div>
+		</Popover>
 	</div>
 {/if}
