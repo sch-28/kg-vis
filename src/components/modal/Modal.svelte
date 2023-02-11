562029e11b6a5f4c537f93728bb9cf5883f2832f
sch-28 <jansch2803@googlemail.com>
2023-02-11 22:53:37 +0100

    refactor: modal background

--- src/components/modal/Modal.svelte
+++ src/components/modal/Modal.svelte
@@ -13,7 +13,7 @@
 <Modal
 	show={content}
 	on:closed={() => ModalManager.close()}
-	classBg="z-[51] fixed top-0 left-0 w-screen h-screen flex flex-col justify-center bg-black/30 "
+	classBg="z-[51] fixed top-0 left-0 w-screen h-screen flex flex-col justify-center bg-gray-900 bg-opacity-50 dark:bg-opacity-80"
 	classWindowWrap="relative m-2 max-h-full"
 	classWindow="relative w-fit max-w-full max-h-full my-2 mx-auto bg-white dark:bg-dark-bg shadow-lg dark:border-dark-muted border rounded-lg "
 	classContent="relative p-4 overflow-auto"
